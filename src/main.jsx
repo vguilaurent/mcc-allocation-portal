7f3c52b618df1ef4a43fc33d7ac698bc67eeeb6a
vguilaurent <vguilaurent@gmail.com>
2025-09-30 10:44:13 -0600

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import AllocationPortal from './portal'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AllocationPortal />
+  </React.StrictMode>,
+)
